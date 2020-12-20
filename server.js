b8e219d076fba3d42f44f0139684b53108b11588
andree1804 <andree1804@gmail.com>
2020-12-20 09:00:19 -0500

    cambios

--- server.js
+++ server.js
@@ -6,16 +6,6 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
-const port = process.env.PORT || 8080;
-
-app.listen(port, function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-        open(`http://localhost:${port}`);
-    }
-});
-
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
