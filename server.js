066e03378cd7fb2878057ffe64a017877c78487f
andree1804 <andree1804@gmail.com>
2020-12-20 09:22:47 -0500

    cambios

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 console.log('erick2');
 app.set('port', process.env.PORT || 80);
-console.log('erick3');
+console.log(app.get('port'));
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
